1f219a3107526d6c68b1c3a4b838733a6ca42258
BartoszWisny <261708@student.pwr.edu.pl>
2025-05-18 23:29:31 +0200

    Modified compilation file for Linux

--- LinuxCompilation/compile.sh
+++ LinuxCompilation/compile.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-
 pip install conan
 conan profile detect --force
-conan install . --output-folder=build --build=missing
-cd build
-cmake .. -DCMAKE_TOOLCHAIN_FILE=conan_toolchain.cmake -DCMAKE_BUILD_TYPE=Release
+rm -rf ../build
+mkdir ../build
+conan install . --output-folder=../build --build=missing
+cd ../build
+cmake ../LinuxCompilation -DCMAKE_TOOLCHAIN_FILE=conan_toolchain.cmake -DCMAKE_BUILD_TYPE=Release
 cmake --build .
